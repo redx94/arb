32358e1789e67c78986f28155545fdab9cbe1cd1
Reece Dixon <quantascriptor@gmail.com>
2025-02-10 02:07:04 -0500

    Updated profitCalculator.ts to enforce numeric consistency

--- src/utils/profitCalculator.ts
+++ src/utils/profitCalculator.ts
@@ -2,8 +2,8 @@ import type { Trade } from '../../types';
 
 export function calculateProfit(trade: Trade): number {
   if (trade.type === 'SELL\') {
-    return trade.price * trade.amount - (trade.gasCost || 0);
+    return trade.amount * trade.price - (trade.gasCost || 0);
   } else {
-    return -(trade.price * trade.amount + (trade.gasCost || 0));
+    return -(trade.amount * trade.price + (trade.gasCost || 0));
   }
 }
